2c535f0018cfe9f9aa5cfccdde8bcd16c636f2b7
Naraharirahul <nmalaya@clemson.edu>
2021-03-05 02:45:22 -0500

    Added ground truth calculation function

--- src/VO.cpp
+++ src/VO.cpp
@@ -16,13 +16,41 @@
 
 
 
-double getAbsoluteScale(int fram_id, int sequence_id, double z_cal)
-{
-    string line;
-    int i = 0;
-    ifstream myfile ("")
+double getAbsoluteScale(int frame_id, int sequence_id, double z_cal)	{
+  
+  string line;
+  int i = 0;
+  ifstream myfile ("/home/rahul/Datasets/KITTI_VO/00.txt");
+  double x =0, y=0, z = 0;
+  double x_prev, y_prev, z_prev;
+  if (myfile.is_open())
+  {
+    while (( getline (myfile,line) ) && (i<=frame_id))
+    {
+      z_prev = z;
+      x_prev = x;
+      y_prev = y;
+      std::istringstream in(line);
+      //cout << line << '\n';
+      for (int j=0; j<12; j++)  {
+        in >> z ;
+        if (j==7) y=z;
+        if (j==3)  x=z;
+      }
+      
+      i++;
+    }
+    myfile.close();
+  }
 
-} 
+  else {
+    cout << "Unable to open file";
+    return 0;
+  }
+
+  return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
+
+}
 void featureTracking(cv::Mat img_1, cv::Mat img_2, std::vector<cv::Point2f>& points1, std::vector<cv::Point2f>& points2,
                      std::vector<uchar>& status)	
 { 
